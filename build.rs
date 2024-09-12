96bed258d347e9f7cf0db3a2aeb4a0be4631b8ec
Troy Dai <git@troydai.com>
2024-09-11 22:56:20 -0700

    Update file location

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 extern crate prost_build;
 
 fn main() {
-    prost_build::compile_protos(&["src/data.proto"], &["src/"])
+    prost_build::compile_protos(&["src/proto/data.proto"], &["src/"])
         .expect("Failed to compile protobuf files");
 }
